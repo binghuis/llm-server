ede477816d7488982156f09ccbe7a7b59103d600
宋秉徽 <451000936@qq.com>
2024-05-08 17:19:38 +0800

    feat: prompt

---
+++ src/server/prompt_templates/test.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful AI bot. Your name is {name}."),
+        ("human", "Hello, how are you doing?"),
+        ("ai", "I'm doing well, thanks!"),
+        ("human", "{user_input}"),
+    ]
+)
+
+messages = chat_template.format_messages(name="Bob", user_input="What is your name?")
+
+if __name__ == "__main__":
+    pprint(chat_template)
