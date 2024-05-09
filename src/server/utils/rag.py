fb3bc7693963bade47d1333fe67af48925ffa420
宋秉徽 <451000936@qq.com>
2024-05-09 13:45:32 +0800

    feat: rag test

--- src/server/utils/rag.py
+++ src/server/utils/rag.py
@@ -16,18 +16,16 @@ langchain_api_key = getenv("LANGCHAIN_API_KEY")
 
 # 1. Indexing: Load
 # 使用 bs4 从 HTML 中提取数据
-bs4_strainer = SoupStrainer(class_=("post-title", "post-header", "post-content"))
+bs4_strainer = SoupStrainer(class_=("devsite-page-title", "devsite-article-body"))
 # 创建网络资源加载器，解析器为 lxml，parse_only 参数控制文档部分解析
 loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    web_paths=(
+        "https://developer.chrome.com/docs/chromium/renderingng-data-structures?hl=zh-cn",
+    ),
     bs_kwargs={"parse_only": bs4_strainer},
     default_parser="lxml",
 )
 docs = loader.load()
-
-print(len(docs[0].page_content))
-
-print(docs[0].page_content[:500])
 
 # 2. Indexing: Split
 text_splitter = RecursiveCharacterTextSplitter(
@@ -35,26 +33,17 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 all_splits = text_splitter.split_documents(docs)
 
-print(len(all_splits), len(all_splits[0].page_content))
-print(all_splits[10].metadata)
-
-
 # 3. Indexing: Store
 vectorstore = Chroma.from_documents(documents=all_splits, embedding=azure_embedding.llm)
 
 # 4. Retrieval and Generation: Retrieve
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
-retrieved_docs = retriever.invoke("你好?")
-print(len(retrieved_docs))
-print(retrieved_docs[0].page_content)
 
 # 5. Retrieval and Generation: Generate
 prompt = hub.pull("rlm/rag-prompt")
 example_messages = prompt.invoke(
     {"context": "filler context", "question": "filler question"}
 ).to_messages()
-
-print(example_messages[0].content)
 
 
 def format_docs(docs):
@@ -68,7 +57,7 @@ rag_chain = (
     | StrOutputParser()
 )
 
-for chunk in rag_chain.stream("What is Task Decomposition?"):
+for chunk in rag_chain.stream("帧树是什么？"):
     print(chunk, end="", flush=True)
 
 if __name__ == "__main__":
